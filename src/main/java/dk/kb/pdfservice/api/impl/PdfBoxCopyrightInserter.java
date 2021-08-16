821444cf4319917a83379ed245522ccab7d8cb12
petchris <pech@kb.dk>
2021-08-16 15:27:25 +0200

    Problem version

---
+++ src/main/java/dk/kb/pdfservice/api/impl/PdfBoxCopyrightInserter.java
@@ -0,0 +1,61 @@
+package dk.kb.pdfservice.api.impl;
+
+import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.state.RenderingMode;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PdfBoxCopyrightInserter {
+
+    public static PDDocument insertCopyrightFooter(File input, File output)
+            throws IOException {
+        PDFParser parser;
+        try (final RandomAccessBufferedFileInputStream rabfis = new RandomAccessBufferedFileInputStream(input)) {
+            parser = new PDFParser(rabfis);
+            parser.parse();
+        }
+
+        final PDDocument doc = parser.getPDDocument();
+
+        boolean initial = true;
+        for (PDPage p : doc.getPages()) {
+            if (initial) { //nasty way of skipping first page
+                initial = false;
+                continue;
+            }
+
+            PDRectangle mediaBox = p.getMediaBox();
+            PDRectangle cropbox = p.getCropBox();
+
+            float ratio = mediaBox.getHeight() / PDRectangle.A4.getHeight();
+            float footer_height = 15 * ratio * p.getUserUnit();
+
+            mediaBox.setLowerLeftY(mediaBox.getLowerLeftY() - footer_height);
+            p.setMediaBox(mediaBox);
+            cropbox.setLowerLeftY(cropbox.getLowerLeftY() - footer_height);
+            p.setCropBox(cropbox);
+
+            try (var contentStream = new PDPageContentStream(doc, p, PDPageContentStream.AppendMode.PREPEND, true);) {
+                contentStream.setRenderingMode(RenderingMode.FILL);
+                contentStream.beginText();
+                contentStream.setFont(PDType1Font.COURIER, footer_height);
+
+                final float x = p.getMediaBox().getWidth() * 0.10f;
+                final float y = -footer_height * 1.2f; //above 100% due to compensation for font height
+                contentStream.newLineAtOffset(x, y);
+                contentStream.showText("Copyright footer");
+                contentStream.endText();
+            }
+        }
+
+        doc.save(output);
+        return doc;
+    }
+}
--- src/main/java/dk/kb/pdfservice/api/impl/PdfServiceApiServiceImpl.java
+++ src/main/java/dk/kb/pdfservice/api/impl/PdfServiceApiServiceImpl.java
@@ -1,49 +1,45 @@
 package dk.kb.pdfservice.api.impl;
 
 import dk.kb.pdfservice.api.*;
+import dk.kb.pdfservice.model.ErrorDto;
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 
-import java.io.*;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-
-import dk.kb.pdfservice.cachingtransformerfactory.SingletonCachingTransformerFactory;
-import dk.kb.pdfservice.config.ServiceConfig;
 import dk.kb.pdfservice.webservice.exception.ServiceException;
 import dk.kb.pdfservice.webservice.exception.InternalServiceException;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.fop.apps.*;
-import org.apache.pdfbox.io.MemoryUsageSetting;
-import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+import java.io.File;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.core.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Providers;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
+import javax.ws.rs.core.MediaType;
+import org.apache.cxf.jaxrs.model.wadl.Description;
+import org.apache.cxf.jaxrs.model.wadl.DocTarget;
 import org.apache.cxf.jaxrs.ext.MessageContext;
+import org.apache.cxf.jaxrs.ext.multipart.*;
 
-import org.w3c.dom.Document;
-import org.apache.pdfbox.pdmodel.PDDocument;
-// New
-import org.xml.sax.SAXException;
-
-import javax.xml.transform.*;
-import javax.xml.transform.sax.SAXResult;
-import javax.xml.transform.stream.StreamSource;
+import io.swagger.annotations.Api;
 
 /**
  * pdf-service
@@ -54,9 +50,6 @@ import javax.xml.transform.stream.StreamSource;
 public class PdfServiceApiServiceImpl implements PdfServiceApi {
     private Logger log = LoggerFactory.getLogger(this.toString());
 
-    // Setup directories
-    File baseDir = new File(".");
-    File outDir = new File(baseDir, "out");
 
 
     /* How to access the various web contexts. See https://cxf.apache.org/docs/jax-rs-basics.html#JAX-RSBasics-Contextannotations */
@@ -100,6 +93,8 @@ public class PdfServiceApiServiceImpl implements PdfServiceApi {
      * 
      * @param barcode: Barcode for a theater manuscript
      * 
+     * @param pdflink: pdfLink for a theater manuscript
+     * 
      * @return <ul>
       *   <li>code = 200, message = "A pdf with attached page", response = File.class</li>
       *   </ul>
@@ -110,23 +105,50 @@ public class PdfServiceApiServiceImpl implements PdfServiceApi {
     @Override
     public javax.ws.rs.core.StreamingOutput getManuscript(String barcode, String pdflink) throws ServiceException {
         // TODO: Implement...
+    
         
         try{ 
             httpServletResponse.setHeader("Content-Disposition", "inline; filename=\"filename.ext\"");
-            String doc = getRawManuscript((barcode));
-     //       return output -> output.write("Magic".getBytes(java.nio.charset.StandardCharsets.UTF_8));
-            return output -> output.write(doc.getBytes(java.nio.charset.StandardCharsets.UTF_8));
+            return output -> output.write("Magic".getBytes(java.nio.charset.StandardCharsets.UTF_8));
         } catch (Exception e){
             throw handleException(e);
         }
+    
     }
 
+    /**
+     * Request a Pdf file from link.
+     * 
+     * @param barcode: Barcode for a theater manuscript
+     * 
+     * @param pdflink2: Pdf for a theater manuscript
+     * 
+     * @return <ul>
+      *   <li>code = 200, message = "A pdf with attached page", response = File.class</li>
+      *   </ul>
+      * @throws ServiceException when other http codes should be returned
+      *
+      * @implNote return will always produce a HTTP 200 code. Throw ServiceException if you need to return other codes
+     */
+    @Override
+    public javax.ws.rs.core.StreamingOutput getPdf(String barcode, String pdflink2) throws ServiceException {
+        // TODO: Implement...
+    
+        
+        try{ 
+            httpServletResponse.setHeader("Content-Disposition", "inline; filename=\"filename.ext\"");
+            return output -> output.write("Magic".getBytes(java.nio.charset.StandardCharsets.UTF_8));
+        } catch (Exception e){
+            throw handleException(e);
+        }
+    
+    }
 
     /**
      * Request a theater manuscript summary.
      * 
-     * @param barCode : Barcode for a theater manuscript
-     *
+     * @param barcode: Barcode for a theater manuscript
+     * 
      * @return <ul>
       *   <li>code = 200, message = "A pdf with attached page", response = String.class</li>
       *   </ul>
@@ -135,165 +157,18 @@ public class PdfServiceApiServiceImpl implements PdfServiceApi {
       * @implNote return will always produce a HTTP 200 code. Throw ServiceException if you need to return other codes
      */
     @Override
-    public String getRawManuscript(String barCode) throws ServiceException {
-        // TODO: Implement...barcode=130018972949
-        final String USER_AGENT = "Mozilla/5.0";
-        String response = null;
-        try {
-            String urlString = "https://soeg.kb.dk/view/sru/45KBDK_KGL?version=1.2&operation=searchRetrieve&query=barcode=" + barCode + "&recordSchema=marcxml";
-            URL url = new URL(urlString);
-
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(new URL(urlString).openStream());
-            doc.getDocumentElement().normalize();
-            System.out.println("Document type: " + doc.getDoctype());
-             return transformToString(doc);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public String getRawManuscript(String barcode) throws ServiceException {
+        // TODO: Implement...
+    
+        
+        try{ 
+            String response = "ft4rUkv";
+        return response;
+        } catch (Exception e){
             throw handleException(e);
         }
+    
     }
-
-    public String transformToString(Document doc) throws TransformerException {
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer xform = transformerFactory.newTransformer();
-        ByteArrayOutputStream bao = new ByteArrayOutputStream();
-
-        xform.transform(new DOMSource(doc), new StreamResult(bao));
-        return bao.toString(StandardCharsets.UTF_8);
-    }
-
-public void convertToPdf(String barCode) throws TransformerException, SAXException, IOException {
-    // the XSL FO file
-    File xsltFile = new File(ServiceConfig.getResourcesDir() + "//formatter.xsl");
-
-    String response = getRawManuscript(barCode);
-    System.out.println("Response: " + response);
-    StreamSource xmlSource =  new StreamSource(new StringReader(response));
-
-    FopFactoryBuilder builder = new FopFactoryBuilder(new File(".").toURI());
-    builder.setAccessibility(true);
-    FopFactory fopFactory = builder.build();
-
-    FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
-    OutputStream outStream = null;
-    Fop fop = null;
-    Result result = null;
-    File pdfOut = new File((ServiceConfig.getOutputDir() + "//" + barCode + ".pdf"));
-    try {
-        outStream = new BufferedOutputStream(new FileOutputStream(pdfOut));
-        fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, outStream);
-
-        // TransformerFactory factory = TransformerFactory.newInstance(); // Old alt. adaption
-        TransformerFactory factory = SingletonCachingTransformerFactory.newInstance();   // NEW adaption
-        Transformer xslfoTransformer = factory.newTransformer(new StreamSource(xsltFile));
-
-        result = new SAXResult(fop.getDefaultHandler());
-
-        // everything will happen here..
-        xslfoTransformer.transform(xmlSource, result);
-    }
-     finally {
-        outStream.close();
-    }
-}
-
-
-/**
- * Request a theater manuscript summary in pdf format.
- *
- * @param barcode : code to get xml result tree String containing info about pdf
- * @param pdflink2 : Relative path including .pdf file
- *
- * @return <ul>
- *   <li>code = 200, message = "A pdf with attached page", response = String.class</li>
- *   </ul>
- * @throws ServiceException when other http codes should be returned
- *
- * @implNote return will always produce a HTTP 200 code. Throw ServiceException if you need to return other codes
- **/
-    @Override
-
-    public StreamingOutput getPdf(String barcode, String pdflink2) {
-        String basepath;
-
-        System.out.println("barcode: " + barcode);
-        System.out.println("pdfLink2: " + pdflink2);
-        basepath = ServiceConfig.getBasepath();
-        System.out.println("basepath: " + basepath);
-        String resourcesDir = ServiceConfig.getResourcesDir();
-        System.out.println("Resources dir: " + resourcesDir );
-        String outputDir = ServiceConfig.getOutputDir();
-
-        if (pdflink2 == null)
-            pdflink2 = "";
-
-        httpServletResponse.setHeader("Content-disposition", " filename = " + pdflink2);
-
-        try {
-            convertToPdf(barcode);
-            System.out.println("mergePDFile(" + outputDir + barcode + ".pdf" + "," + pdflink2 +")");
-            // mergePDFFile(outputDir + "output.pdf",pdflink2);
-            mergePDFFile(outputDir + barcode + ".pdf",pdflink2); // TEST
-            final InputStream inputStream = new FileInputStream(new File((outputDir + "//" + barcode + ".pdf")));
-            return output ->  {
-                IOUtils.copy(inputStream, output);
-            };
-        } catch ( TransformerException | SAXException| IOException e ) {
-            e.printStackTrace();
-            throw new InternalServiceException("Fejl med getPdf", e);
-        }
-    }
-
-    /**
-     * Request a theater manuscript summary in pdf format.
-     *
-     * @param apron : name of  xml result tree String containing info about pdf
-     * @param pdfFile : Relative path including .pdf file
-     *
-     * @return <ul>
-     *   <li>code = 200, message = "A pdf apron and with attached pages", response = String.class</li>
-     *   </ul>
-     * @throws ServiceException when other http codes should be returned
-     *
-     * @implNote return will always produce a HTTP 200 code. Throw ServiceException if you need to return other codes
-     **/
-
-    public StreamingOutput getMergePDFs(@NotNull String apron, @NotNull String pdfFile) {
-    return  null; }
-
-    private void mergePDFFile(String apron, @NotNull String pdfFile) {
-    // public StreamingOutput mergePDFs(String apron, String pdfFile) {
-        String outputDir = ServiceConfig.getOutputDir();
-
-        String resourcesDir = ServiceConfig.getResourcesDir();
-        // File file1 = new File(outputDir + "output.pdf");
-        File file1 = new File( apron);
-        File file2 = new File(resourcesDir + pdfFile);
-
-        //Instantiating PDFMergerUtility class
-        PDFMergerUtility PDFmerger = new PDFMergerUtility();
-        System.out.println(("APRON in mergePDFFile: " + apron));
-        //Setting the destination file
-        PDFmerger.setDestinationFileName( apron );
-
-        httpServletResponse.setHeader("Content-disposition", " filename = merged_" + pdfFile);
-        //adding the source files
-        try {
-            PDFmerger.addSource(file1);
-            PDFmerger.addSource(file2);
-            //Merging the two documents
-            System.out.println("merging the two documents: " + file1 + " and " + file2 + ")");
-            PDFmerger.mergeDocuments(MemoryUsageSetting.setupMixed(1024 * 1024 * 500));
-        } catch ( IOException e) {
-            e.printStackTrace();
-            throw new InternalServiceException("Fejl med merge af forside og pdf", e);
-        }
-        System.out.println("Documents merged");
-        //return  PDFmerger.getDestinationStream();
-    }
-
 
     /**
      * Ping the server to check if the server is reachable.
@@ -310,8 +185,10 @@ public void convertToPdf(String barCode) throws TransformerException, SAXExcepti
     @Override
     public String ping() throws ServiceException {
         // TODO: Implement...
+    
+        
         try{ 
-            String response = "e7nJq";
+            String response = "o9W165";
         return response;
         } catch (Exception e){
             throw handleException(e);
@@ -319,11 +196,12 @@ public void convertToPdf(String barCode) throws TransformerException, SAXExcepti
     
     }
 
+
     /**
     * This method simply converts any Exception into a Service exception
     * @param e: Any kind of exception
     * @return A ServiceException
-    *  dk.kb.webservice.ServiceExceptionMapper
+    * @see dk.kb.webservice.ServiceExceptionMapper
     */
     private ServiceException handleException(Exception e) {
         if (e instanceof ServiceException) {
